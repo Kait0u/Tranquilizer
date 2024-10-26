098e6bf76558ffe140ea4a2a51da2fbb5d437acd
Jakub Jaworski <jjaw.stuff@gmail.com>
2024-10-26 17:12:57 +0200

    Minor fixes

--- tranq_train.py
+++ tranq_train.py
@@ -6,7 +6,7 @@ from slugify import slugify
 from shared.init_activities import initialization_activities
 
 
-def __train(name: str | None, epochs: int, batch_size: int, data_dir: str, data_limit: int, grayscale: bool):
+def __train(name: str | None, epochs: int, batch_size: int, data_dir: str, data_limit: int, grayscale: bool, save_every=10):
     import torch
     from torch.utils.data import DataLoader
     from backend.ds import DenoisingDataset
@@ -32,7 +32,8 @@ def __train(name: str | None, epochs: int, batch_size: int, data_dir: str, data_
         device=device,
         num_epochs=epochs,
         gsc=grayscale,
-        name=name
+        name=name,
+        save_every_epochs=save_every,
     )
 
 
@@ -44,12 +45,17 @@ def main():
     parser.add_argument("--batch_size", type=int, default=32, help="Size of the batches (default: 32)")
     parser.add_argument("--data_dir", type=str, required=True, help="Path to the data directory. It must contain images.")
     parser.add_argument("--data_limit", type=int, default=0, help="Limit on the amount of data (default: 0 - unlimited)")
+    parser.add_argument("--checkpoint_every", type=int, default=10, help="Save a checkpoint every n epochs (default: 10)")
     parser.add_argument("--grayscale", action="store_true", help="Split images into 3 1-channel images instead of processing them as 3-channel images")
+
 
     args = parser.parse_args()
 
     if args.epochs < 1:
         parser.error("epochs must be at least 1.")
+
+    if args.checkpoint_every < 1:
+        parser.error("\"Checkpoint every\" must be positive.")
 
     if not os.path.isdir(args.data_dir):
         parser.error(f"The data directory '{args.data_dir}' does not exist.")
@@ -66,10 +72,19 @@ def main():
     print(f"Epochs: {args.epochs}")
     print(f"Batch size: {args.batch_size}")
     print(f"Data directory: {args.data_dir}")
-    print(f"Data limit: {args.data_limit}" + " (unlimited)" if args.data_limit == 0 else "")
-    print("Channel Split Mode: " + "YES" if args.grayscale else "NO")
+    print(f"Data limit: {args.data_limit}" + (" (unlimited)" if args.data_limit == 0 else ""))
+    print(f"Checkpoint every: {args.checkpoint_every}")
+    print("Channel Split Mode: " + ("YES" if args.grayscale else "NO"))
     print()
-    __train(name=args.name, epochs=args.epochs, batch_size=args.batch_size, data_dir=args.data_dir, data_limit=args.data_limit, grayscale=args.grayscale)
+    __train(
+        name=args.name,
+        epochs=args.epochs,
+        batch_size=args.batch_size,
+        data_dir=args.data_dir,
+        data_limit=args.data_limit,
+        grayscale=args.grayscale,
+        save_every=args.checkpoint_every,
+    )
 
 
 if __name__ == "__main__":
